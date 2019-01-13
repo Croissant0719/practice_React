c402bbf5bc2c2ccc0d40620c37b24bf990c994cf
Koji Igarashi <koji.igarashi.0719@gmail.com>
2019-01-13 23:18:39 +0900

    Add Tic-Toc-Toe game

---
+++ app/javascript/packs/react/tic_toc_toes/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Game from "./index/game";
+import "./app.css";
+
+ReactDOM.render(
+  <Game />,
+  document.body.appendChild(document.createElement("div"))
+);
