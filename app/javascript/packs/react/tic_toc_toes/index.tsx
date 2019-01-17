29440c6e0270f4dab9b5ef23f839a3ff91855745
Koji Igarashi <koji.igarashi.0719@gmail.com>
2019-01-17 10:54:26 +0900

    Fix CSS

--- app/javascript/packs/react/tic_toc_toes/index.tsx
+++ app/javascript/packs/react/tic_toc_toes/index.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Game from "./index/game";
-// import "./app.css";
+
+// view で stylesheet_link_tag で読んでいるため実は必要ない
+import "./app.scss";
 
 ReactDOM.render(
   <Game />,
