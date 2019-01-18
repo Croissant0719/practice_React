7076e554a7b167831330706f55f0bb4b9cd79e87
Koji Igarashi <koji.igarashi.0719@gmail.com>
2019-01-18 23:49:55 +0900

    Add ToDo View

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'hello_reacts' => 'hello_reacts#index'
   get 'tic_toc_toes' => 'tic_toc_toes#index'
+  get 'to_dos' => 'to_dos#index'
 end
