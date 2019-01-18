5a7f00cf73f1cd7b0e1a19f00d6d9b0bfda12522
Koji Igarashi <koji.igarashi.0719@gmail.com>
2019-01-18 23:44:02 +0900

    Add CSS

--- app/javascript/packs/react/hello_reacts/index.tsx
+++ app/javascript/packs/react/hello_reacts/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import HelloReact from "./index/hello_react";
-import "./app.css";
+// import "./app.css";
 
 ReactDOM.render(
   <HelloReact name="Koji Igarashi" />,
