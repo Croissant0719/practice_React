8ee368847c59699975e8973ed96f459edd41f2e1
Koji Igarashi <koji.igarashi.0719@gmail.com>
2019-01-19 00:17:20 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'reacts#index'
+
   get 'hello_reacts' => 'hello_reacts#index'
   get 'tic_toc_toes' => 'tic_toc_toes#index'
-  get 'to_dos' => 'to_dos#index'
+  get 'to_dos'       => 'to_dos#index'
 end
