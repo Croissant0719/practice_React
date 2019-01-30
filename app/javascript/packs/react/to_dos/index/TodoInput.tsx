717bab50d5f4f47cadd9e2feab02ccbcf62a8264
Koji Igarashi <koji.igarashi.0719@gmail.com>
2019-01-30 23:15:55 +0900

    Add event handle 'handleClick'

--- app/javascript/packs/react/to_dos/index/TodoInput.tsx
+++ app/javascript/packs/react/to_dos/index/TodoInput.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
 
 class TodoInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.props.addTodo('新規Todo');
+  }
+
   render() {
     return (
       <div>
         <input placeholder="新規ToDoを入力してください" />
-        <button>登録</button>
+        <button onClick={this.handleClick} >登録</button>
       </div>
     );
   }
